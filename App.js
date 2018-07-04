83fcc8479c2826f8a19df2801f9b707a9bdb24c7
jake-unchained <jakebwalsh@gmail.com>
2018-07-04 13:37:04 +0100

    added some basic button functionality and removed the app from a wrapper to make it truly native

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import FetchLocation from './components/FetchLocation'
+
 export default class App extends React.Component {
+
+  getUserLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position);
+    }, err => console.log(err));
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FetchLocation onGetLocation={this.getUserLocationHandler} />
       </View>
     );
   }
